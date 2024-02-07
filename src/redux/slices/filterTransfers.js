c92f56d7f06eb925bdf7bf2a4ba405d06d943786
Nikolay <149093882+NMKRTYCHIAN@users.noreply.github.com>
2024-02-07 14:55:33 +0300

    add filterTransfers

--- src/redux/slices/filterTransfers.js
+++ src/redux/slices/filterTransfers.js
@@ -13,11 +13,38 @@ const filterTransfers = createSlice({
   initialState,
   reducers: {
     setFilterCheckbox(state, action) {
-      return state.map((filterName) =>
+      if (action.payload === 'all') {
+        if (state[0].checked === true) {
+          return state.map((filterName) =>
+            filterName.checked !== true
+              ? filterName
+              : { ...filterName, checked: !filterName.checked },
+          )
+          // eslint-disable-next-line no-else-return
+        } else {
+          return state.map((filterName) =>
+            filterName.checked === true
+              ? filterName
+              : { ...filterName, checked: !filterName.checked },
+          )
+        }
+      }
+
+      const newState = state.map((filterName) =>
         filterName.filter === action.payload
           ? { ...filterName, checked: !filterName.checked }
           : filterName,
       )
+
+      if (!newState.every((el) => el.checked)) {
+        newState[0] = { ...newState[0], checked: false }
+      }
+
+      if (newState.slice(1).every((el) => el.checked)) {
+        newState[0] = { ...newState[0], checked: true }
+      }
+
+      return newState
     },
   },
 })
