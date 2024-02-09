e116470b1d4ac5ce54dffee3761f1a7d0bee568e
Nikolay <149093882+NMKRTYCHIAN@users.noreply.github.com>
2024-02-09 12:26:29 +0300

    refactoring

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
 import { useSelector } from 'react-redux'
 import { Alert, Spin } from 'antd'
@@ -7,45 +8,43 @@ import TicketsList from '../TicketsList/TicketsList'
 import logo from '../../icon/Logo.svg'
 import './App.scss'
 
-export default function App() {
+const LoadingMessage = ({ message, type }) => (
+  <Alert
+    message={message}
+    type={type}
+    style={{
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      marginTop: '20px',
+      width: '500px',
+    }}
+  />
+)
+
+const App = () => {
   const stopSearch = useSelector((state) => state.ticketsData.stop)
 
   return (
     <div className="App">
       <div className="title">
-        {' '}
-        {/* Заголовок и лого */}
         <img className="logo" src={logo} alt="logo" />
         <h1>Welcome to AviaKataSales</h1>
         <hr />
-        {/* То что будет показываться, пока билеты загражются */}
-        {!stopSearch && <Spin size="large" style={{ marginTop: '30px' }} />}
         {!stopSearch && (
-          <Alert
-            message="Билеты подгружаются, но часть из них вы уже можете посмотреть!"
-            type="warning"
-            style={{
-              marginLeft: 'auto',
-              marginRight: 'auto',
-              marginTop: '20px',
-              width: '500px',
-            }}
-          />
+          <>
+            <Spin size="large" style={{ marginTop: '30px' }} />
+            <LoadingMessage
+              message="Билеты подгружаются, но часть из них вы уже можете посмотреть!"
+              type="warning"
+            />
+          </>
         )}
-        {/* То что будет показываться, после загрузки билетов */}
         {stopSearch && (
-          <Alert
+          <LoadingMessage
             message="Все билеты успешно загружены, приятного путешествия!"
             type="success"
-            style={{
-              marginLeft: 'auto',
-              marginRight: 'auto',
-              marginTop: '20px',
-              width: '500px',
-            }}
           />
         )}
-        {/* Основные компоненты */}
       </div>
       <FilterTransfer />
       <FilterTabsTickets />
@@ -53,3 +52,5 @@ export default function App() {
     </div>
   )
 }
+
+export default App
--- src/components/Ticket/Ticket.jsx
+++ src/components/Ticket/Ticket.jsx
@@ -1,66 +1,64 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
+import { v4 } from 'uuid'
 import formatTime from '../../utils/formatTime'
 import wordEndings from '../../utils/wordEndings'
 import cl from './Ticket.module.scss'
 
-export default function Ticket({ ticket }) {
-  const { price, carrier, segments } = ticket
+const FlightSegment = ({ segment }) => {
+  return (
+    <span>
+      <span className={cl.opaci}>
+        {segment.origin} - {segment.destination}
+      </span>
+      <span>{formatTime(segment)}</span>
+    </span>
+  )
+}
 
+const FlightInfo = ({ segment }) => {
+  return (
+    <span>
+      <span className={cl.opaci}>В пути</span>
+      <span>
+        {Math.floor(segment.duration / 60)}ч {segment.duration % 60}мин
+      </span>
+    </span>
+  )
+}
+
+const FlightStops = ({ stops }) => {
+  return (
+    <span>
+      <span className={cl.opaci}>
+        {stops.length === 0 ? '' : stops.length} {wordEndings(stops.length)}
+      </span>
+      <span>{stops.join(', ')}</span>
+    </span>
+  )
+}
+
+const Ticket = ({ ticket }) => {
+  const { price, carrier, segments } = ticket
   const logoUrl = `//pics.avs.io/99/36/${carrier}.png`
 
   return (
     <div className={cl.ticket}>
       <span className={cl.price}>{price} Р</span>
       <img className={cl.logo} src={logoUrl} alt="logo" />
-
       <div className={cl.flights}>
         <div className={cl.rows}>
-          <div className={cl['row-top']}>
-            <span>
-              <span className={cl.opaci}>
-                {segments[0].origin} - {segments[0].destination}
-              </span>
-              <span>{formatTime(segments[0])}</span>
-            </span>
-            <span>
-              <span className={cl.opaci}>В пути</span>
-              <span>
-                {Math.floor(segments[0].duration / 60)}ч {segments[0].duration % 60}мин
-              </span>
-            </span>
-            <span>
-              <span className={cl.opaci}>
-                {segments[0].stops.length === 0 ? '' : segments[0].stops.length}{' '}
-                {wordEndings(segments[0].stops.length)}
-              </span>
-              <span>{segments[0].stops.join(', ')}</span>
-            </span>
-          </div>
-
-          <div className={cl['row-bottom']}>
-            <span>
-              <span className={cl.opaci}>
-                {segments[1].origin} - {segments[1].destination}
-              </span>
-              <span>{formatTime(segments[1])}</span>
-            </span>
-            <span>
-              <span className={cl.opaci}>В пути</span>
-              <span>
-                {Math.floor(segments[1].duration / 60)}ч {segments[1].duration % 60}мин
-              </span>
-            </span>
-            <span>
-              <span className={cl.opaci}>
-                {segments[1].stops.length === 0 ? '' : segments[1].stops.length}{' '}
-                {wordEndings(segments[1].stops.length)}
-              </span>
-              <span>{segments[1].stops.join(', ')}</span>
-            </span>
-          </div>
+          {segments.map((segment, index) => (
+            <div key={v4()} className={index === 0 ? cl['row-top'] : cl['row-bottom']}>
+              <FlightSegment segment={segment} />
+              <FlightInfo segment={segment} />
+              <FlightStops stops={segment.stops} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
   )
 }
+
+export default Ticket
